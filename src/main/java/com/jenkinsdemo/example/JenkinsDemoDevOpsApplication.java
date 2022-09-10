858b60660befa58a8bc4ee083f57c6a47ed06d18
jigar <jigar@DESKTOP-L9UAV8D>
2022-09-10 10:50:23 +0530

    First Commit

---
+++ src/main/java/com/jenkinsdemo/example/JenkinsDemoDevOpsApplication.java
@@ -0,0 +1,28 @@
+package com.jenkinsdemo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinsDemoDevOpsApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsDemoDevOpsApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoDevOpsApplication.class, args);
+	}
+	
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String welcome() {
+		return "Hello, This is Jigar Rathod";
+	}
+}
---
+++ src/test/java/com/jenkinsdemo/example/JenkinsDemoDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsdemo.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
